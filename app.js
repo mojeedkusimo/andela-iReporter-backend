f007f85790a91c47cc80f0e30b89cce654052a17
MOJEED KUSIMO <mkusimo90@gmail.com>
2021-04-03 10:55:22 +0100

    Setup app server

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const PORT = process.env.PORT
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    return res.send("Welcome to Andela Project: iReporter");
+})
+
+app.use((req, res, next) => {
+    let error = new Error("Page Not Found");
+    res.status = 404;
+
+    return next(error);
+})
+
+app.use((err, req, res, next) => {
+    res.status = err.status || 500;
+
+    return res.json({
+        message: err.message,
+        error: err
+
+    })
+})
+
+app.listen(PORT, () => {
+    console.log("Server running on port..." + PORT);
+})
